a1b1575f853ee39224158135ac5da3bd669e4454
Eric <eric.rodrig.gomes@gmail.com>
2026-05-28 00:32:58 -0300

    Mudanças na parte da organização,  há somente um erro esquisito na criação da sala

---
+++ Navegaçao/Itens/item.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var id = ""
+@export var item_name = ""
+@export var is_interactable = false
+
+#TODO
+func _input_event(viewport: Viewport, event: Input_event, shape_idx: int):
+	
+func interact():
+	
+func change_frame(idx: int):
+	
---
+++ Navegaçao/navigation_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var current_room : Node = null
+func _ready() -> void:
+	$right_arrow.pressed.connect(_on_right_arrow_pressed)
+	$left_arrow.pressed.connect(_on_left_arrow_pressed)
+	var sala_1_tscn = preload("res://Navegaçao/Rooms/room_1.tscn")
+	var instancia_sala = sala_1_tscn.instantiate()
+	add_child(instancia_sala) 
+	current_room = instancia_sala
+	
+func _on_right_arrow_pressed():
+	if current_room != null:
+		current_room.mudar_parede(1)
+	else:
+		print("Erro: Nenhuma sala atual definida!")
+
+func _on_left_arrow_pressed():
+	if current_room != null:
+		current_room.mudar_parede(-1)
+	else:
+		print("Erro: Nenhuma sala atual definida!")
