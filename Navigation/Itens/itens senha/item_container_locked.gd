a6f0880c22af1b1a9076c8730cb5bc467021d3bf
Eric <eric.rodrig.gomes@gmail.com>
2026-06-26 21:55:56 -0300

    Corrigir alguma coisa

--- Navigation/Itens/itens senha/item_container_locked.gd
+++ Navigation/Itens/itens senha/item_container_locked.gd
@@ -2,16 +2,23 @@ extends item_container
 
 # conferir se e o mesmo da tranca
 @export var id_senha = ""
+@export var id_lock = ""
+
+func _ready():
+	super()
+	for filho in get_children():
+		if filho is item && filho.id == id_lock:
+			filho.visible = true
 
 func change_container():
-	if open:
+	if open || ProgressionManager.get_status_senha(id_senha):
 		super()
+		for filho in get_children():
+			if filho is item && filho.id == id_lock:
+				filho.visible = !open
+		
 	else:
-		if ProgressionManager.get_status_senha(id_senha):
-			super()
-		else:
-			alertar_tranca()
-	
+		alertar_tranca()
 	
 # TODO: mecanismo para alertar que esta trancado
 func alertar_tranca():
