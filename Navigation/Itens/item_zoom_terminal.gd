2b36135967be37b169bf98adbf04e8ad9a5303e2
Eric <eric.rodrig.gomes@gmail.com>
2026-06-27 21:30:25 -0300

    O guilherne entendeu, logo o problema é outro.... (ajeitando alguma coisas e preparando para os pendrives)

--- Navigation/Itens/item_zoom_terminal.gd
+++ Navigation/Itens/item_zoom_terminal.gd
@@ -1,17 +1,24 @@
 extends item_zoom
 
 var ligado = false
+var pendrive = 0
+#TODO: terminar as mudanças do pendrive
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
 
-
 func _on_pressed() -> void:
+	var item_selecionado = Inventory.hotbar[Inventory.selected_slot]
 	if ligado:
-		super.open_zoom()
+		if item_selecionado:
+			if item_selecionado.item_name == "pendrive_1":
+				pendrive = 1
+			if pendrive == 1 and item_selecionado.item_name == "pendrive_2":
+				pendrive = 2
+		else:
+			super.open_zoom()
 	else:
-		var item_selecionado = Inventory.hotbar[Inventory.selected_slot]
 		if item_selecionado and item_selecionado.item_name == "Cabo":
 			ligado = true
 			Inventory.remove_item(item_selecionado)
